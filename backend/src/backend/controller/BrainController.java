5b1a6d98a205d9c54a9e95f92a8c4769ca1b192f
Federica Di Lauro <federicadilauro1998@gmail.com>
2019-06-22 13:26:25 +0200

    user inutile

--- backend/src/backend/controller/BrainController.java
+++ backend/src/backend/controller/BrainController.java
@@ -1,5 +1,19 @@
 package backend.controller;
 
-public class BrainController {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
+public class BrainController {
+	
 }
--- backend/src/backend/repository/JobRepository.java
+++ backend/src/backend/repository/JobRepository.java
@@ -1,5 +1,46 @@
 package backend.repository;
+import backend.model.Job;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JobRepository {
 
+	private Map<String, Job> jobs;
+
+	private static JobRepository instance = null;
+
+	public static JobRepository getInstance() {
+		if (instance == null) {
+			instance = new JobRepository();
+		}
+		return instance;
+	}
+
+	private JobRepository() {
+		this.jobs = new HashMap<>();
+	}
+
+	public Collection<Job> findAll() {
+		return this.jobs.values();
+	}
+
+	
+	public Job find(String job_id) {
+		return this.jobs.get(job_id);
+	}
+
+	public void save(Job job) {
+		this.jobs.put(job.getJob_id(), job);
+	}
+
+
+	public Job update(String job_id, Job job) {
+		return this.jobs.replace(job_id, job);
+	}
+
+	public Job delete(String job_id) {
+		return this.jobs.remove(job_id);
+	}
 }
